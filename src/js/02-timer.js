2dba97e45bbb02fdbe2c5e1d6837acbb2a6ee1d5
HennadiiBorysevych <genuch3@gmail.com>
2023-02-03 21:00:53 +0200

    second task

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,21 +1,53 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const btnStart = document.querySelector('button[data-start]');
+const days = document.querySelector('span[data-days]');
+const hours = document.querySelector('span[data-hours]');
+const minutes = document.querySelector('span[data-minutes]');
+const seconds = document.querySelector('span[data-seconds]');
+
+let timerId = null;
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-    console.log(selectedDates[0].getTime() - Date.now());
+    const startTime = selectedDates[0].getTime();
+
+    if (startTime - Date.now() < 0) {
+      Notify.info('Please choose a date in the future');
+      btnStart.setAttribute('disabled', 'true');
+      clearInterval(timerId);
+    } else {
+      btnStart.removeAttribute('disabled');
+    }
+
+    btnStart.addEventListener('click', () => {
+      clearInterval(timerId);
+
+      timerId = setInterval(() => {
+        const currentTime = Date.now();
+
+        const timeDifference = currentTime - startTime;
+        const timeConverted = convertMs(timeDifference);
+        console.log(timeConverted);
+
+        days.textContent = Math.abs(timeConverted.days);
+        hours.textContent = Math.abs(timeConverted.hours);
+        minutes.textContent = Math.abs(timeConverted.minutes);
+        seconds.textContent = Math.abs(timeConverted.seconds);
+      }, 1000);
+    });
   },
 };
-const picker = flatpickr('#datetime-picker', options);
-console.log(picker.selectedDates[0].getTime() - Date.now());
 
-const btnStart = document.querySelector('button[data-start]');
+const picker = flatpickr('#datetime-picker', options);
 
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
@@ -32,7 +64,3 @@ function convertMs(ms) {
 
   return { days, hours, minutes, seconds };
 }
-
-// console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-// console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-// console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
