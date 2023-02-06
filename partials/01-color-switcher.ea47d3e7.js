08167723f9ead2663a814161aceac408d968d25a
HennadiiBorysevych <112689590+HennadiiBorysevych@users.noreply.github.com>
2023-02-06 13:06:01 +0000

    Deploying to gh-pages from @ HennadiiBorysevych/goit-js-hw-09@601adb9609066ef630d2db82da0fb03edd4359fd 🚀

--- partials/01-color-switcher.ea47d3e7.js
+++
@@ -1,2 +0,0 @@
-!function(){var t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=null;t.addEventListener("click",(function(){t.setAttribute("disabled","true"),e.removeAttribute("disabled"),n=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),e.addEventListener("click",(function(){e.setAttribute("disabled","true"),t.removeAttribute("disabled"),clearInterval(n)}))}();
-//# sourceMappingURL=01-color-switcher.ea47d3e7.js.map
